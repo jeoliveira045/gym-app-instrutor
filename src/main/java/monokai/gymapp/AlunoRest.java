0430e2ef79be601d6c0e5ab168b0934183db455c
jeoliveira045 <jean.mateus.1997@gmail.com>
2025-06-05 23:55:22 -0300

    Implementando service

--- src/main/java/monokai/gymapp/AlunoRest.java
+++ src/main/java/monokai/gymapp/AlunoRest.java
@@ -4,6 +4,7 @@ import monokai.gymapp.domain.model.Aluno;
 import monokai.gymapp.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class AlunoRest {
     @GetMapping
     public List<Aluno> findAll(){
         return alunoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Aluno findById(@PathVariable Long id) {
+        return alunoService.findById(id);
     }
 }
--- src/main/java/monokai/gymapp/services/AlunoService.java
+++ src/main/java/monokai/gymapp/services/AlunoService.java
@@ -5,9 +5,13 @@ import monokai.gymapp.domain.model.Treino;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AlunoService {
@@ -22,18 +26,56 @@ public class AlunoService {
                 """
                 SELECT 
                     aluno_id, nome, data_nascimento, altura, peso, imc 
-                FROM ALUNO""", new BeanPropertyRowMapper<>(Aluno.class)).stream().map(aluno -> {
-                    List<Treino> treinoList = jdbcTemplate.query("""
-                            SELECT * FROM TREINO WHERE ALUNO_ID = ?
-                            """, new Object[]{aluno.getAlunoId()}, (rs, rowNum) -> {
-                                    var treino = new Treino();
-                                    treino.setTreinoId(rs.getLong("treino_id"));
-                                    treino.setNomeTreino(rs.getString("nome_treino"));
-                                    return treino;
-                    });
-                    aluno.setTreinoList(treinoList);
-                    return aluno;
-        }).toList();
+                FROM ALUNO""", new BeanPropertyRowMapper<>(Aluno.class));
+    }
+
+    public Aluno findById(Long id){
+        String alunoquery = """
+                SELECT 
+                    aluno_id, nome, data_nascimento, altura, peso, imc 
+                FROM ALUNO WHERE ALUNO_ID = ?
+                """;
+        String treinoQuery = """
+                SELECT
+                    treino_id, nome_treino from
+                TREINO WHERE aluno_id = ?
+                """;
+        var aluno = jdbcTemplate.queryForObject(alunoquery,  new BeanPropertyRowMapper<>(Aluno.class), id);
+        jdbcTemplate.queryForList(treinoQuery, id).forEach(treinos -> {
+            Treino treino = new Treino();
+            Arrays.stream(treino.getClass().getDeclaredFields()).forEach(field -> {
+                field.setAccessible(true);
+//                try {
+                String fieldName = field.getName();
+                String newFieldName = "";
+                Pattern pattern = Pattern.compile("[A-Z]");
+                List<Character> characterList = getCharacters(fieldName, pattern);
+                for(Character chara: characterList){
+                    newFieldName += chara.toString();
+                }
+                fieldName = newFieldName.toLowerCase(Locale.ROOT);
+                System.out.println(fieldName);
+
+            });
+
+
+        });
+        return aluno;
+    }
+
+    private static List<Character> getCharacters(String fieldName, Pattern pattern) {
+        char[] charList = fieldName.toCharArray();
+        List<Character> characterList = new ArrayList<>();
+        for(char chara: charList){
+            characterList.add(chara);
+        }
+        characterList.forEach(character -> {
+            Matcher matcher = pattern.matcher(character.toString());
+            if(matcher.find()){
+                characterList.add(characterList.indexOf(character), '_');
+            }
+        });
+        return characterList;
     }
 
 }
